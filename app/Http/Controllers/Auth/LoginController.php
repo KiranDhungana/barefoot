6f85dd22a9536ed69e5eb2f86a33830a17e7101e
KiranDhungana <kirandhungana570@gmail.com>
2023-11-14 10:01:52 +0545

    logout view account

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\account;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -42,22 +44,42 @@ class LoginController extends Controller
     public function login(Request $req)
     {
         $input = $req->all();
-        $this->validate(
-            $req,
-            [
-                'email' => 'required|email',
-                'password' => 'required',
-            ]
-        );
+        // $this->validate(
+        //     $req,
+        //     [
+        //         'email' => 'required|email',
+        //         'password' => 'required',
+        //     ]
+        // );
+        $loginid = $input['email'];
+
         if (auth()->attempt(array('email' => $input['email'], 'password' => $input['password']))) {
             if (auth()->user()->is_admin == 1) {
                 $d = User::all();
-
                 return redirect()->route('admin-home');
                 // return view('admin', ['data' => $d]);
+            } else if (auth()->user()) {
+                return redirect()->route('home');
             }
-            return redirect()->route('home');
+
+        } else if (account::where('loginid', '=', $loginid)->first()) {
+            $loginid = $input['email'];
+            $password = $input['password'];
+
+            $user = account::where('loginid', '=', $loginid)->first();
+
+            if (!$user) {
+                return response()->json(['success' => false, 'message' => 'Login Fail, please check email id']);
+            }
+            if (!Hash::check($password, $user->password)) {
+                return response()->json(['success' => false, 'message' => 'Login Fail, pls check password']);
+            }
+
+            return view('viewaccount')->with('accountdata', $user);
+
+        } else {
+
+            return redirect()->route('login')->with('errormsg', 'Invalid credentials');
         }
-        return redirect()->route('login')->with('errormsg', 'Invalid credentials');
     }
 }
--- resources/views/viewaccount.blade.php
+++ resources/views/viewaccount.blade.php
@@ -1,73 +1,101 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-     <style>
-      .card {
-  /* Add shadows to create the "card" effect */
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-  transition: 0.3s;
-  width: fit-content;
-  padding-left: 25px;
-  padding-right: 50px;
-  padding-top: 30px;
-  padding-bottom: 30px;
-}
+    <style>
+        .card {
+            /* Add shadows to create the "card" effect */
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+            transition: 0.3s;
+            width: fit-content;
+            padding-left: 25px;
+            padding-right: 50px;
+            padding-top: 30px;
+            padding-bottom: 30px;
+        }
 
-/* On mouse-over, add a deeper shadow */
-.card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-}
+        /* On mouse-over, add a deeper shadow */
+        .card:hover {
+            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+        }
 
-/* Add some padding inside the card container */
-.con {
-  padding-left: 5px;
-  padding:5px; 
+        /* Add some padding inside the card container */
+        .con {
+            padding-left: 5px;
+            padding: 5px;
 
-}
-     </style>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    @include('adminnav')
-  </head>
-  <body>
-  <div  style="margin-left: 50px ;margin-top:10px; margin-right:50px;" >
-    <div class="card">
-  <img src="/images/avatar.jpg" alt="Avatar" style="width:60px">
-  <div class="con">
-    Name: {{ucfirst(trans($accountdata->name))}} 
-    <br>
-     Center: {{$accountdata->center}} Branch
-     <br>
-    Due Amount:  <span style="color: red" >{{$accountdata->fee}}  </span>  
-    <br>
-    Rank: {{$accountdata->rank}}
-    <br>
-     Latest paid Amount: <span style="color: green" > {{$accountdata->paid}} </span>
-     <br>
-      Latest Paid Date: {{$accountdata->paiddate}}
-      <br>
-       Total Paid Amount: <span style="color: green" > {{$accountdata->totalpaidamount}}   </span>
- 
-  </div>
-</div>
- 
- <br> 
+        }
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    {{-- @include('adminnav') --}}
+</head>
 
- <br>    
- 
+<body>
+    <div style="margin-left: 50px ;margin-top:10px; margin-right:50px;">
+        <div class="card">
+            <img src="/images/avatar.jpg" alt="Avatar" style="width:60px">
+            <div class="con">
+                <div style="display: flex">
+                    Name: {{ ucfirst(trans($accountdata->name)) }}
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
 
- <br>    
+                    </a>
 
- <br>    
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+           document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
 
- <br>    
-  
-</div> 
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </div>
 
-      
-   
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-  </body>
+
+            
+                Center: {{ $accountdata->center }} Branch
+                <br>
+                Due Amount: <span style="color: red">{{ $accountdata->fee }} </span>
+                <br>
+                Rank: {{ $accountdata->rank }}
+                <br>
+                Latest paid Amount: <span style="color: green"> {{ $accountdata->paid }} </span>
+                <br>
+                Latest Paid Date: {{ $accountdata->paiddate }}
+                <br>
+                Total Paid Amount: <span style="color: green"> {{ $accountdata->totalpaidamount }} </span>
+                <br>
+
+
+            </div>
+        </div>
+
+        <br>
+
+        <br>
+
+
+        <br>
+
+        <br>
+
+        <br>
+
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
